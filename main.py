d842744d312b8720187e6b7e09ddc66000ec683e
omriweinmann <omri.weinmann@gmail.com>
2026-01-23 00:30:04 -0700

    b

--- main.py
+++ main.py
@@ -5,3 +5,28 @@ class Test:
 new_test = Test("bob")
 
 print(new_test.name)
+
+print("""\
+_ _ _ _ _ o o o o _ _ o o o o o
+_ _ _ o _ _ _ o o _ o o o o o o
+_ _ _ o _ _ o _ _ _ o o o o o o
+_ o _ o _ _ o o o o o o o o o o
+_ _ _ _ _ o o o o o o o o o o o
+_ _ _ _ o o o o o o o o o o o o
+_ _ _ o o o o o o o o o o o o o
+_ _ _ _ o o o o o o o o o _ _ o
+_ o o o o _ _ o o _ o o _ _ _ _
+_ o o o o _ _ _ o _ o _ o o o o
+_ o o o _ _ _ _ o _ o _ o o o o
+_ _ _ _ o o o _ _ _ _ _ _ _ _ o
+_ _ o o o o o o _ _ _ _ _ _ o o
+_ o o o o o o o o _ o o o _ o o
+_ o o o o o o o o o o o o o _ o
+o o o o o o o o o o o o o o _ _    
+                    """)
+
+
+
+
+
+
