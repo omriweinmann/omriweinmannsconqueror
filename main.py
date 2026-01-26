3fec072ac3c4aa96663f5ffff683e6f50efba544
omriweinmann <omri.weinmann@gmail.com>
2026-01-26 11:31:29 -0700

    Map and Faction localization

--- main.py
+++ main.py
@@ -2,22 +2,121 @@ class Localization:
     def __init__(self, array):
         self.array = array
         
-language = "Sp"
+language = "En"
         
 localization = {
+    "reb_idl" : {"En" : "x", "Sp" : "x"},
+    "reb_idu" : {"En" : "X", "Sp" : "X"},
+    "reb_name" : {"En" : "Natives", "Sp" : "Nativos"},
+    "reb_adj" : {"En" : "Native", "Sp" : "Nativo/a"},
+    
     "eng_idl" : {"En" : "e", "Sp" : "e"},
     "eng_idu" : {"En" : "E", "Sp" : "E"},
-    "eng_name" : {"En" : "Great Britain", "Sp" : "Gran Bretaña"},
-    "eng_adj" : {"En" : "British", "Sp" : "Británico/a,"}
-    "cul_weo" : {"En" : "Western European", "Sp" : "de Europeo Occidental" }
+    "eng_name" : {"En" : "Britain", "Sp" : "Bretaña"},
+    "eng_adj" : {"En" : "British", "Sp" : "Británico/a,"},
+   
+    "den_idl" : {"En" : "d", "Sp" : "d"},
+    "den_idu" : {"En" : "D", "Sp" : "D"},
+    "den_name" : {"En" : "Denmark", "Sp" : "Dinamarca"},
+    "den_adj" : {"En" : "Danish", "Sp" : "Danés/esa"},
+    
+    "swe_idl" : {"En" : "s", "Sp" : "s"},
+    "swe_idu" : {"En" : "S", "Sp" : "S"},
+    "swe_name" : {"En" : "Sweden", "Sp" : "Suecia"},
+    "swe_adj" : {"En" : "Swedish", "Sp" : "Sueco/a"},
+    
+    "rus_idl" : {"En" : "r", "Sp" : "r"},
+    "rus_idu" : {"En" : "R", "Sp" : "R"},
+    "rus_name" : {"En" : "Russia", "Sp" : "Rusia"},
+    "rus_adj" : {"En" : "Russian", "Sp" : "Ruso/a"},
+    
+    "pol_idl" : {"En" : "l", "Sp" : "l"},
+    "pol_idu" : {"En" : "L", "Sp" : "L"},
+    "pol_name" : {"En" : "Poland", "Sp" : "Polonia"},
+    "pol_adj" : {"En" : "Polish", "Sp" : "Polaco/a"},
+    
+    "pru_idl" : {"En" : "p", "Sp" : "p"},
+    "pru_idu" : {"En" : "P", "Sp" : "P"},
+    "pru_name" : {"En" : "Prussia", "Sp" : "Prusia"},
+    "pru_adj" : {"En" : "Prussian", "Sp" : "Prusio/a"},
+    
+    "ger_idl" : {"En" : "g", "Sp" : "g"},
+    "ger_idu" : {"En" : "G", "Sp" : "G"},
+    "ger_name" : {"En" : "German States", "Sp" : "Estados Alemanes"},
+    "ger_adj" : {"En" : "German", "Sp" : "Alemán/ana"},
+    
+    "fra_idl" : {"En" : "f", "Sp" : "f"},
+    "fra_idu" : {"En" : "F", "Sp" : "F"},
+    "fra_name" : {"En" : "France", "Sp" : "Francia"},
+    "fra_adj" : {"En" : "French", "Sp" : "Francés/esa"},
+    
+    "aus_idl" : {"En" : "a", "Sp" : "a"},
+    "aus_idu" : {"En" : "A", "Sp" : "A"},
+    "aus_name" : {"En" : "Austria", "Sp" : "Austria"},
+    "aus_adj" : {"En" : "Austrian", "Sp" : "Austriaco/a"},
+    
+    "ita_idl" : {"En" : "i", "Sp" : "i"},
+    "ita_idu" : {"En" : "I", "Sp" : "I"},
+    "ita_name" : {"En" : "Italian States", "Sp" : "Estados Italianos"},
+    "ita_adj" : {"En" : "Italian", "Sp" : "Italiano/a"},
+    
+    "ott_idl" : {"En" : "o", "Sp" : "o"},
+    "ott_idu" : {"En" : "O", "Sp" : "O"},
+    "ott_name" : {"En" : "Turkey", "Sp" : "Turquía"},
+    "ott_adj" : {"En" : "Ottoman", "Sp" : "Otomano/a"},
+    
+    "spa_idl" : {"En" : "ñ", "Sp" : "ñ"},
+    "spa_idu" : {"En" : "Ñ", "Sp" : "Ñ"},
+    "spa_name" : {"En" : "Spain", "Sp" : "España"},
+    "spa_adj" : {"En" : "Spanish", "Sp" : "Español/a"},
+    
+    "por_idl" : {"En" : "t", "Sp" : "t"},
+    "por_idu" : {"En" : "T", "Sp" : "T"},
+    "por_name" : {"En" : "Portugal", "Sp" : "Portugal"},
+    "por_adj" : {"En" : "Portuguese", "Sp" : "Portugués/esa"},
+    
+    "pap_idl" : {"En" : "†", "Sp" : "†"},
+    "pap_idu" : {"En" : "✚", "Sp" : "✚"},
+    "pap_name" : {"En" : "Papal States", "Sp" : "Estados Pontificios"},
+    "pap_adj" : {"En" : "Papal", "Sp" : "Pontificio/a"},
+    
+    "mor_idl" : {"En" : "m", "Sp" : "m"},
+    "mor_idu" : {"En" : "M", "Sp" : "M"},
+    "mor_name" : {"En" : "Morocco", "Sp" : "Marruecos"},
+    "mor_adj" : {"En" : "Moroccan", "Sp" : "Marroquí"},
+    
+    "cul_brb" : {"En" : "Barbarian", "Sp" : "Bárbaro"},
+    "cul_weu" : {"En" : "Western European", "Sp" : "Europeo Occidental" },
+    "cul_eeu" : {"En" : "Eastern European", "Sp" : "Europa del Este"},
+    "cul_isl" : {"En" : "Islamic", "Sp" : "Islam"},
 }
+
+start = [
+    [
+        "_ _ _ _ _ d d s s _ _ r r r r +",
+        "_ _ _ e _ _ _ s s _ r r r r r +",
+        "_ _ _ e _ _ d _ _ _ l l r r r +",
+        "_ e _ e _ _ d p p p p l l l r +",
+        "_ _ _ _ _ g g g p l l l l l r +",
+        "_ _ _ _ f f g g p p a l l l r r",
+        "_ _ _ f f f f + a a a a l l r r"
+        "_ _ _ _ f f i a a a a o o _ _ r",
+        "_ ñ ñ ñ ñ _ _ i i _ o o _ _ _ _",
+        "_ t ñ ñ ñ _ _ _ † _ o _ o o o o",
+        "_ t ñ ñ _ _ _ _ i _ o _ o o o o",
+        "_ _ _ _ o o o _ _ _ _ _ _ _ _ o",
+        "_ _ m m + + + o _ _ _ _ _ _ o o",
+        "_ m m + + + + o o _ o o o _ o o",
+        "_ x + + + + + + o o o o o o _ o",
+        "x + + + + + + + + + + o o o _ _"
+    ]
+]
 
 def get_localed(key):
     return localization[key][language]
     
 def get_dual_localed(key, key2):
     return localization[key + "_" + key2][language]
-
         
 class Faction:
     def __init__(self, ide, namedef, attributes):
@@ -26,34 +125,36 @@ class Faction:
         self.attributes = attributes
         
 class Attributes:
-    def __init__(self,culture assimilation)
+    def __init__(self,culture, assimilation):
         self.culture = culture
         self.assimilation = assimilation
 
-new_test = Faction("e", "eng", Attributes("cul_weu", 4))
+factions = {
+    "x" : Faction("x", "reb", Attributes("cul_brb",4)),
+    "e" : Faction("e", "eng", Attributes("cul_weu",4)),
+    "d" : Faction("d", "den", Attributes("cul_weu",2)),
+    "s" : Faction("s", "swe", Attributes("cul_eeu",2)),
+    "r" : Faction("r", "rus", Attributes("cul_eeu",1)),
+    "l" : Faction("l", "pol", Attributes("cul_eeu",2)),
+    "p" : Faction("p", "pru", Attributes("cul_weu",2)),
+    "g" : Faction("g", "ger", Attributes("cul_weu",2)),
+    "f" : Faction("f", "fra", Attributes("cul_weu",2)),
+    "a" : Faction("a", "aus", Attributes("cul_weu",2)),
+    "i" : Faction("i", "ita", Attributes("cul_weu",2)),
+    "o" : Faction("o", "ott", Attributes("cul_isl",1)),
+    "ñ" : Faction("ñ", "spa", Attributes("cul_weu",2)),
+    "t" : Faction("t", "por", Attributes("cul_weu",2)),
+    "†" : Faction("†", "pap", Attributes("cul_weu",2)),
+    "m" : Faction("m", "mor", Attributes("cul_isl",2))
+}
 
-print(get_dual_localed(new_test.namedef, "adj"))
-
-print("""\
-_ _ _ _ _ o o o o _ _ o o o o o
-_ _ _ o _ _ _ o o _ o o o o o o
-_ _ _ o _ _ o _ _ _ o o o o o o
-_ o _ o _ _ o o o o o o o o o o
-_ _ _ _ _ o o o o o o o o o o o
-_ _ _ _ o o o o o o o o o o o o
-_ _ _ o o o o o o o o o o o o o
-_ _ _ _ o o o o o o o o o _ _ o
-_ o o o o _ _ o o _ o o _ _ _ _
-_ o o o o _ _ _ o _ o _ o o o o
-_ o o o _ _ _ _ o _ o _ o o o o
-_ _ _ _ o o o _ _ _ _ _ _ _ _ o
-_ _ o o o o o o _ _ _ _ _ _ o o
-_ o o o o o o o o _ o o o _ o o
-_ o o o o o o o o o o o o o _ o
-o o o o o o o o o o o o o o _ _    
-                    """)
-
-
+def get_faction_local(idl,key):
+    return get_dual_localed(factions[idl].namedef, key)
+    
+print(get_faction_local("†", "name")) # prints "Papal States"
+print(get_faction_local("t", "adj")) # prints "Portuguese"
+print(get_faction_local("e", "idu")) # prints "E"
+print(get_faction_local("o", "idl")) # prints "o"
 
 
 
